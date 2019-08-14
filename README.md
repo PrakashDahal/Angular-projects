0a6fbeea12e3ea961d9f432bf3d464c77dac9256
Prakash <dahalprakash1720@gmail.com>
2019-08-14 22:57:21 +0545

    Responsive as well as added social media icons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PrakashEportfolio
+# PrakashDahalProfile
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prakash-eportfolio'`, () => {
+  it(`should have as title 'prakashDahal-profile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('prakash-eportfolio');
+    expect(app.title).toEqual('prakashDahal-profile');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to prakash-eportfolio!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to prakashDahal-profile!');
   });
 });
